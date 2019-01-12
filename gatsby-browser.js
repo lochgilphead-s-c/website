12b0c6d06d6057575e8417197d5071fdfdaa537b
Patrick Grey <websemantic@yahoo.co.uk>
2019-01-12 22:43:19 +0000

    Add update confirmation

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+exports.onServiceWorkerUpdateFound = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
