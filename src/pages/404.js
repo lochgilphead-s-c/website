cfd0b7db4bfb400afa8891a2ce82f52231313d10
Patrick Grey <websemantic@yahoo.co.uk>
2019-02-28 11:01:12 +0000

    Updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Logo from "../img/logo/LSC_logo-double-small.fw.png";
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+  <div>
+    <a className="navbar-brand js-scroll-trigger" href="/index.html">
+      <img className="lsc-logo" src={Logo} alt="Lochgilphead Soccer Centre" />
+    </a>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
